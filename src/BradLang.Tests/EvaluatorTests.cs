2659a05a4d4d80f3f6b58623f193b35be8641cab
Bradley Ford <github@bradleyford.id.au>
2018-10-26 21:35:46 +1100

    Added Evaluator unit tests

---
+++ src/BradLang.Tests/EvaluatorTests.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using BradLang.CodeAnalysis.Syntax;
+using Xunit;
+
+namespace BradLang.Tests
+{
+    public class EvaluatorTests
+    {
+        [Theory]
+        [InlineData("1", 1)]
+        [InlineData("-1", -1)]
+        [InlineData("1 + 2", 3)]
+        [InlineData("17 - 9", 8)]
+        [InlineData("2 * 7", 14)]
+        [InlineData("21 / 7", 3)]
+        [InlineData("(1 + 2) * 3", 9)]
+        [InlineData("(540)", 540)]
+        [InlineData("true", true)]
+        [InlineData("false", false)]
+        [InlineData("!true", false)]
+        [InlineData("!false", true)]
+        [InlineData("10 == 10", true)]
+        [InlineData("50 != 12", true)]
+        [InlineData("true && true", true)]
+        [InlineData("true || false", true)]
+        [InlineData("true && false", false)]
+        [InlineData("\"Hello\"", "Hello")]
+        [InlineData("\"Hello, \" + \"World!\"", "Hello, World!")]
+        [InlineData("\"Hello\" == \"Hello\"", true)]
+        [InlineData("\"Hello\" == \"World\"", false)]
+        [InlineData("\"Hello\" != \"Hello\"", false)]
+        [InlineData("\"Hello\" != \"World\"", true)]
+        [InlineData("a = 42", 42)]
+        [InlineData("a = b = 10", 10)]
+        public void Evaluator_Evaluate(string text, object expectedValue)
+        {
+            var syntaxTree = SyntaxTree.Parse(text);   
+            
+            var compilation = new Compilation(syntaxTree);
+            
+            var variables = new Dictionary<VariableSymbol, object>();
+
+            var result = compilation.Evaluate(variables);
+
+            Assert.Empty(result.Diagnostics);
+            Assert.Equal(expectedValue, result.Value);
+        }
+    } 
+
+}
